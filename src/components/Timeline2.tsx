057f65fd6ae9f7fa903b068af99e77761e7adec9
Taufik Oktama <ot@unforma.club>
2023-10-07 13:40:55 +0700

    fix: Improve timeline structures

--- src/components/Timeline2.tsx
+++ src/components/Timeline2.tsx
@@ -3,7 +3,12 @@
 import moment from "moment";
 import works from "@/database/works.json";
 import { cn } from "@/libs/utils";
-import { useMemo, useState } from "react";
+import { useMemo } from "react";
+
+function nthDate(date: string) {
+    return moment(date).format("MMM Do, YYYY");
+    // .replace(/(\d)(st|nd|rd|th)/g, "$1<sup>$2</sup>");
+}
 
 type Work = (typeof works)[0];
 
@@ -25,18 +30,21 @@ function Info(props: InfoProps) {
                 <span>{props.label}</span>
                 <span>:</span>
             </div>
-            <div className={cn("col-span-3 lg:col-span-4")}>{props.value}</div>
+            <div
+                className={cn("col-span-3 lg:col-span-4")}
+                dangerouslySetInnerHTML={{ __html: props.value }}
+            />
         </div>
     );
 }
 
 function Detail(props: Work) {
-    const { project, description, date, principle, employer, category, role, scope } = props;
-    const newEnd = date.end === "now" ? new Date().toISOString() : date.end;
+    const { project, description, date, principle, employer, category, role, scope, link } = props;
+    const isPresent = date.end === "now";
 
     return (
-        <li className={cn("mb-24")}>
-            <div className={cn("mb-12", "flex flex-col", "gap-y-6")}>
+        <li>
+            <div className={cn("mb-6", "flex flex-col", "gap-y-6")}>
                 <div>
                     <div
                         className={cn(
@@ -45,10 +53,28 @@ function Detail(props: Work) {
                             "leading-none"
                         )}
                     >
-                        {project}
+                        <span
+                            dangerouslySetInnerHTML={{
+                                __html: nthDate(date.start)
+                            }}
+                        />
+                        <span>&nbsp;</span>
+                        <span>—</span>
+                        <span>&nbsp;</span>
+                        <span
+                            dangerouslySetInnerHTML={{
+                                __html: isPresent ? "Now" : nthDate(date.end)
+                            }}
+                        />
                     </div>
-                    <div className={cn("text-sm lg:text-2xl", "text-neutral-500")}>
-                        {moment(date.start).format("ll")} — {moment(newEnd).format("ll")}
+                    <div
+                        className={cn(
+                            "text-4xl lg:text-6xl",
+                            "font-serif font-bold",
+                            "leading-none"
+                        )}
+                    >
+                        {project}
                     </div>
                 </div>
 
@@ -58,6 +84,12 @@ function Detail(props: Work) {
                     <Info label="Category" value={category} />
                     <Info label="Role" value={role} />
                     <Info label="Scope" value={scope} />
+                    {link && (
+                        <Info
+                            label="Link"
+                            value={`<a href="${link}" target="_blank" rel="nofollow noopener noreferrer" class="text-cyan-500 hover:underline">${link}</a>`}
+                        />
+                    )}
                 </div>
             </div>
 
@@ -80,7 +112,9 @@ export default function Timeline2() {
         const reduces = works
             .sort((a, b) => b.date.start.localeCompare(a.date.start))
             .reduce((items: { [key: string]: Work[] }, item) => {
-                const year = moment(item.date.start).format("YYYY");
+                const isPresent = item.date.end === "now";
+                const year = isPresent ? "Now" : moment(item.date.start).format("YYYY");
+
                 if (!items[year]) {
                     items[year] = [];
                 }
@@ -89,13 +123,39 @@ export default function Timeline2() {
             }, {});
 
         return reduces;
-    }, [works]);
+    }, []);
 
     const years = Object.keys(groupByYears);
 
     return (
         <>
-            <ul>
+            <div
+                className={cn(
+                    "sticky",
+                    "top-0",
+                    "h-16",
+                    "z-10",
+                    "bg-neutral-50/90 dark:bg-neutral-950/90",
+                    "overflow-hidden",
+                    "border-b",
+                    "-mb-px",
+                    "grid grid-cols-6 lg:grid-cols-12",
+                    "gap-x-2",
+                    "items-center",
+                    "text-sm lg:text-2xl",
+                    "font-bold"
+                )}
+            >
+                <div className={cn("col-span-1 lg:col-span-2")}>Year</div>
+                <div
+                    className={cn("flex items-center justify-between", "col-span-5 lg:col-span-10")}
+                >
+                    <div>Experience</div>
+                    <div>Sorter</div>
+                </div>
+            </div>
+
+            <ul className={cn("py-8 lg:py-16", "flex flex-col", "gap-y-16")}>
                 {years.reverse().map((year, i) => (
                     <li key={i} className={cn("grid grid-cols-6 lg:grid-cols-12", "gap-x-2")}>
                         <div
@@ -106,8 +166,7 @@ export default function Timeline2() {
                                 className={cn(
                                     "text-2xl lg:text-4xl",
                                     "font-serif font-bold",
-                                    "pb-24",
-                                    "sticky top-12",
+                                    "sticky top-20",
                                     "leading-none"
                                 )}
                             >
@@ -115,7 +174,9 @@ export default function Timeline2() {
                             </div>
                         </div>
 
-                        <ul className={cn("col-span-5 lg:col-span-10")}>
+                        <ul
+                            className={cn("col-span-5 lg:col-span-10", "flex flex-col", "gap-y-16")}
+                        >
                             {groupByYears[year].map((work, i) => (
                                 <Detail key={i} {...work} />
                             ))}
